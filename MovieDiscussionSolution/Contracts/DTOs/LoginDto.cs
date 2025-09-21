082fd2e90a87323c555a605991dfc49e0d200696
dusan003 <dusan003gordanic@gmail.com>
2025-09-21 13:36:45 +0200

    Added needed models and dto classes.
    Co-authored-by: Mihailo39 <stevicmihailo248@gmail.com>

--- MovieDiscussionSolution/Contracts/DTOs/LoginDto.cs
+++ MovieDiscussionSolution/Contracts/DTOs/LoginDto.cs
@@ -1,14 +1,18 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace Contracts.DTOs
+namespace Common.DTOs
 {
-    public class LoginDto
+    public class LoginDTO
     {
-        public string Email { get; set; } = string.Empty;
-        public string Password { get; set; } = string.Empty;
+        [Required(ErrorMessage = "Email is required")]
+        [EmailAddress(ErrorMessage = "Enter a valid email address")]
+        public string Email { get; set; }
+        [Required(ErrorMessage = "Password is required")]
+        public string Password { get; set; }
     }
 }
